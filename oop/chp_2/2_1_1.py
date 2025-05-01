d4bb0fb9fa526d654d8b248ac7633fe983c9f5fd
Anastasia Serguta <anastasia.serguta@gmail.com>
2025-05-01 18:02:02 +0300

    01/05/25

---
+++ oop/chp_2/2_1_1.py
@@ -0,0 +1,22 @@
+class Clock:
+
+    def __init__(self, tm = 0):
+        self.__time = 0
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.__time
+
+    @staticmethod
+    def __check_time(tm):
+        return isinstance(tm, int) and 0 <= tm < 100_000
+
+clock = Clock()
+
+clock.set_time(4530)
+print(clock.get_time())
